419e8f0fa79157023848ef058ebea71713b849d6
easycode07 <akinwumitolulope274@gmail.com>
2025-07-23 17:20:41 +0100

    Addded File

--- src/component/AboutMe.jsx
+++ src/component/AboutMe.jsx
@@ -23,7 +23,7 @@ const AboutMe = () => {
               Get in touch
             </a>
 
-            <a href="/Resume.docx" 
+            <a href="/public/CURRICULUM VITAE.pdf" 
               className={cn("px-6 py-2 rounded-full border border-primary text-primary", 
                "hover:bg-primary/10 transition-colors duration-300")}
                download>
